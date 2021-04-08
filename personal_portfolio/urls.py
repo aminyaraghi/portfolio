dabef787f36eb80563b6a0246a55d3495b126cb5
Amin <amin.yaraghi@gmail.com>
2021-04-08 17:43:51 +0200

    create blog html pages

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
